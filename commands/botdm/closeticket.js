96c24a945156c6343873e0389bae3ee662c1de66
jacksonsouth <34117488+jacksonsouth@users.noreply.github.com>
2021-02-11 00:47:11 -0600

    Update closeticket.js

--- commands/botdm/closeticket.js
+++ commands/botdm/closeticket.js
@@ -1,7 +1,11 @@
 module.exports = {
     commands: ["closeticket"],
     callback(message, arguments, text){
-        if(message.channel.parentID !== "798407665436983326") return;
-        message.channel.send("Thank you for contacting the Sage support team, we hope you received the help you requested for; we are now closing this ticket. **Have a nice day!**");
+        if(message.channel.parentID !== "809313707736694785") return;
+        message.channel.send("Thank you for contacting the Sage support team, we hope you received the help you requested for; this ticket will be closing in 5 seconds. **Have a nice day!**");
+        setTimeout(closeTick, 5000);
+        function closeTick(){
+            message.channel.close();
+        }
     }
 }
